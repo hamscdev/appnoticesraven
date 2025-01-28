4c39f4e8b11a5a47fbed355233c44075a21f31f0
Hector Moreno <hector.morenos@elektra.com.mx>
2025-01-28 10:07:03 -0600

    Initial Commit

---
+++ newyorktimes/newyorktimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  newyorktimesApp.swift
+//  newyorktimes
+//
+//  Created by 1135794 on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct newyorktimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
